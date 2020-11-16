7ce6b90fa713b8e203a381522f459b92675e4120
nicolas.mueller@students.ffhs.ch <nicolas.mueller@students.ffhs.ch>
2020-11-16 11:00:48 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import {RouterModule, Routes} from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
@@ -13,16 +12,16 @@ const appRoutes: Routes = [
 
 ];
 
+// @ts-ignore
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent
   ],
     imports: [
-        BrowserModule,
-        NgbModule,
-        RouterModule.forRoot(appRoutes),
-        BrowserAnimationsModule,
+      BrowserModule,
+      RouterModule.forRoot(appRoutes),
+      BrowserAnimationsModule,
       MatIconModule,
       MatButtonModule
     ],

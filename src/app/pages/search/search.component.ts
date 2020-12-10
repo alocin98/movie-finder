c9cbc2f72fb6d4fa88ba3113db541707cac2218e
nicolas.mueller@students.ffhs.ch <nicolas.mueller@students.ffhs.ch>
2020-12-10 13:46:59 +0100

    time measure

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -14,16 +14,24 @@ export class SearchComponent implements OnInit {
   @HostBinding('class') class = 'search-container';
 
   movies$: Observable<any>;
+  measure = this.getTime();
 
   constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.measure = new Date().getMilliseconds();
+    });
     this.movies$ = this.route.params.pipe(
       switchMap(params => this.http.get('https://api.themoviedb.org/3/search/movie?api_key=a6b2c9accaaa7fb251ad47c6d6c59b5c&language=en-US&query=' + params.query + '&page=1&include_adult=false')),
-      tap(console.log),
+      tap(() => this.measure = this.getTime() - this.measure),
       // @ts-ignore
       map(search => search.results),
     );
+  }
+
+  getTime(){
+    return new Date().getMilliseconds();
   }
 
 }
